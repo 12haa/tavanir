3c314816d09ea643583c58816d8f4684ed408975
12haa <mkhaefi.work@gmail.com>
2026-08-31 16:09:45 +0330

    added iran font to project

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,13 +23,13 @@ function Login() {
         <LoginModal>
           <div className="flex flex-col items-center justify-start gap-20 w-full h-full">
             <div className=" flex flex-col items-center  w-full p-1">
-              <h1
-                className="text-[#ea580c] text-[25px] font-bold"
-                style={{ color: colors.primary[600] }}
-              >
-                سامانه مدیریت پیک بار کشور
-              </h1>
-              <h3>توانیر</h3>
+<h1
+  className="text-[#ea580c] text-[25px] font-bold font-iran"
+  style={{ color: colors.primary[600] }}
+>
+  سامانه مدیریت پیک بار کشور
+</h1>
+<h3 className="font-iran">توانیر</h3>
             </div>
             <div className=" flex flex-col items-center w-full p-1">s</div>
             <div className=" flex flex-col items-center w-full p-1">d</div>
