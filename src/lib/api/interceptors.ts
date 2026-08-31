dca925a53c52cd005b374a077ea316627f4c6349
12haa <mkhaefi.work@gmail.com>
2026-08-31 14:03:30 +0330

    add inital api config file  & interceptor && install required packages

---
+++ src/lib/api/interceptors.ts
@@ -0,0 +1,32 @@
+import type { AxiosError, AxiosInstance, InternalAxiosRequestConfig } from "axios";
+import api from "./api";
+
+// Request interceptor
+// api.interceptors.request.use(
+//   (config: InternalAxiosRequestConfig) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error: AxiosError) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// Response interceptor
+// api.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error: AxiosError) => {
+//     if (error.response?.status === 401) {
+//       localStorage.removeItem("token");
+//       window.location.href = "/login";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default api;
