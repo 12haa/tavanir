4cc07ac16bc6316688fc3f5a35652225c02581bd
12haa <mkhaefi.work@gmail.com>
2026-09-01 12:28:13 +0330

    added NavPanelMenu component & start working on nabigation logic and adding routes object

--- src/components/NavMenuButton.tsx
+++ src/components/NavMenuButton.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { colors } from '../lib/theme';
+import NavMenuPanel from './NavMenuPanel';
 
 function NavMenuButton() {
   const [open, setOpen] = useState(false);
@@ -15,18 +16,7 @@ function NavMenuButton() {
         منو سامانه
       </button>
 
-      <div
-        className={` fixed top-17 z-99 transition-transform duration-300 ease-in-out rounded-2xl shadow-md ${
-          open ? 'right-8' : 'right-0'
-        }`}
-        style={{
-          width: 300,
-          height: 550,
-          backgroundColor: colors.navPanel[100],
-          border: `1px solid ${colors.navPanel[200]}`,
-          transform: open ? 'translateX(0)' : 'translateX(100%)',
-        }}
-      ></div>
+      <NavMenuPanel open={open} />
     </>
   );
 }
